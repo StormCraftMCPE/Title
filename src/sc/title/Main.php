394dc16d99ec0837ebf7439a6261eabe65bea79b
ajo||SC <stormcraftmcpe@gmail.com>
2017-07-29 15:51:02 +0700

    Update Main.php

--- src/sc/title/Main.php
+++ src/sc/title/Main.php
@@ -1,4 +1,23 @@
 <?php
 
-use pocketmine\Player
-use pocketmine\plugin\PluginBase
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+
+Class Main extends PluginBase implements Listener{
+  
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    $this->getLogger()->info("plugin Titler Enable!");}
+  
+  public function onDisable(){
+    $this->getLogger()->info("Titler Disable!");
+    }
+  public function onJoin(PlayerJoinEvent $ev){
+    $player = $ev->getPlayer();
+    $player->addTitle("Welcome To Server");
+    
+    }
+  }
